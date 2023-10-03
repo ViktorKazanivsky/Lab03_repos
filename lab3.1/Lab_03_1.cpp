657dab6b744ed7c26383673dcaa5ff1d1aaaa615
ViktorKazanivsky <viktorkazanivsky@gmail.com>
2023-10-03 22:50:29 +0300

    lab 3.1

---
+++ lab3.1/Lab_03_1.cpp
@@ -0,0 +1,36 @@
+// Lab_03_1.cpp
+// Казанівський Віктор
+// Лабораторна робота № 3.1
+// Розгалуження, задане формулою: функція однієї змінної.
+// Варіант 10
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double A; 
+	double B; 
+	cout << "x = "; cin >> x;
+
+	A = 4.95 * pow(x, 2);
+
+	if (x < -3.5) 
+	B = 4 + pow(x, -2);
+
+	if (x >= -3.5 && x < 1) 
+	B = tan(3.5 + x) / 5;
+
+	if (x >= 1) 
+	B = sin(3 * x) - cos(x);
+
+	y = A + B;
+
+	cout << endl;
+	cout << "y = " << y << endl;
+
+	return 0;
+}
+	
